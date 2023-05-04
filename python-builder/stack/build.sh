c1e3bf4cfb9d63c8b2b358e80d63fa99a10aa9cc
laminar <tpiperatgod@gmail.com>
2023-05-04 21:38:15 +0800

    update stack
    Signed-off-by: laminar <tpiperatgod@gmail.com>

--- python-builder/stack/build.sh
+++ python-builder/stack/build.sh
@@ -3,40 +3,45 @@
 set -euo pipefail
 
 # Set variables
-BASE_IMAGE="ubuntu"
-STACK_NAME="example-stack"
+STACK_NAME="ofnee-stack"
+BASE_IMAGE="${STACK_NAME}-base"
 STACK_VERSION="0.1.0"
 RUN_IMAGE_NAME="${STACK_NAME}-run"
 BUILD_IMAGE_NAME="${STACK_NAME}-build"
+# HTTP_PROXY="http://host.docker.internal:7890"
+# HTTPS_PROXY="http://host.docker.internal:7890"
 
 # Build base image
 echo "Building base image..."
 docker build \
-  -t ${STACK_NAME}-base:${STACK_VERSION} \
-  -f base/Dockerfile \
+  -t ${BASE_IMAGE}:${STACK_VERSION} \
+  -f Dockerfile.base \
   --build-arg BASE_IMAGE=${BASE_IMAGE} \
   --build-arg HTTP_PROXY=${HTTP_PROXY:-} \
   --build-arg HTTPS_PROXY=${HTTPS_PROXY:-} \
+  --network host \
   .
 
 # Build run image
 echo "Building run image..."
 docker build \
   -t ${RUN_IMAGE_NAME}:${STACK_VERSION} \
-  -f run/Dockerfile \
-  --build-arg BASE_IMAGE=${STACK_NAME}-base \
+  -f Dockerfile.run \
+  --build-arg BASE_IMAGE=${BASE_IMAGE}:${STACK_VERSION} \
   --build-arg HTTP_PROXY=${HTTP_PROXY:-} \
   --build-arg HTTPS_PROXY=${HTTPS_PROXY:-} \
+  --network host \
   .
 
 # Build build image
 echo "Building build image..."
 docker build \
   -t ${BUILD_IMAGE_NAME}:${STACK_VERSION} \
-  -f build/Dockerfile \
-  --build-arg BASE_IMAGE=${STACK_NAME}-base \
+  -f Dockerfile.build \
+  --build-arg BASE_IMAGE=${BASE_IMAGE}:${STACK_VERSION} \
   --build-arg HTTP_PROXY=${HTTP_PROXY:-} \
   --build-arg HTTPS_PROXY=${HTTPS_PROXY:-} \
+  --network host \
   .
 
 echo "Done."
