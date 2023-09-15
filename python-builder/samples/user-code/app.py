177b76dc7b0f8e92cdfaa9e71ab12e6ac8646a83
laminar <tpiperatgod@gmail.com>
2023-09-15 17:42:48 +0800

    update builder
    Signed-off-by: laminar <tpiperatgod@gmail.com>

--- python-builder/samples/user-code/app.py
+++ python-builder/samples/user-code/app.py
@@ -1,14 +1,15 @@
-from function_framework import FunctionContext
 from datetime import datetime
 
-def user_function(context: FunctionContext):
-    print(context.request.metadata, flush=True)
-    print(context.request.text(), flush=True)
+from functions_framework.context.user_context import UserContext
+
+
+def user_function(context: UserContext):
+    # context.get_http_request() is a flask request object
+    print(context.get_http_request())
 
     now = datetime.now()
 
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
-
-    output_data = "hello!!!!!!"
+    output_data = "hello world"
     return output_data
